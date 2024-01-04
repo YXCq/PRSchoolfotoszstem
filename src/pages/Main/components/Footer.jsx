5c894e4b35b929f4fe82181248f66450e92040c1
exqusite <109229289+exquisitte@users.noreply.github.com>
2024-01-04 16:30:45 +0200

    ui

--- src/pages/Main/components/Footer.jsx
+++ src/pages/Main/components/Footer.jsx
@@ -1,6 +1,9 @@
 export const Footer = () => {
     return (
         <div className="footer">
+            <h1>Created by DH company</h1>
+            <p>2023 DH company</p>
+            <p>dhcompany028@gmail.com</p>
         </div>
     )
 }
